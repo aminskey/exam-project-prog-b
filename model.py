069a09bedb666cff33d7b24feb94b31e2d326332
johanhede <158845236+johanhede@users.noreply.github.com>
2025-03-12 14:15:33 +0100

    Working on load_from_file

--- model.py
+++ model.py
@@ -1,17 +1,36 @@
 import json
 import requests
 
+from player import Player, Coin
+
 class Model:
     """
-        - Add self.playerDict
-        - Make a function that will update self.playerDict
-        - Make a function that will save self.playerDict in a file
+        [12/03/25: DONE] Add self.playerDict
+        [12/03/25: DONE] Make a function that will update self.playerDict
+        [12/03/25: DONE] Make a function that will save self.playerDict in a file
+        [12/03/25] Make a function that will load playerdata.json into self.__playerDict
     """
     def __init__(self):
         self.__playerDict = {}
 
+
     def updatePlayerData(self, p):
         self.__playerDict[p.name] = p.saveData()
+
+
+    def load_from_file(self):
+        with open("playerdata.json", "r") as f:
+            tmp = json.load(f)
+            f.close()
+        
+        for player in tmp.items():
+            p = Player(tmp[player].name, tmp[player].money)
+            p.history = tmp[player].history
+
+            for coin in tmp["coins"]:
+                c = Coin("", 0)
+                c.type = tmp[player][coin]["type"]
+                # TODO: Finish function...
 
 
     def save_to_file(self):
@@ -32,6 +51,6 @@ class Model:
             return data
         print(f"Error: {response.status_code}")
 
-    # TODO: Implement a method to load playerdata for all players and create Player objects
-    # TODO: Implement a method to save playerdata for all the players
-    # TODO: Add self.controller
+    # TODO: [11-03-25] Implement a method to load playerdata for all players and create Player objects
+    # TODO: [11-03-25] Implement a method to save playerdata for all the players
+    # TODO: [11-03-25] Add self.controller
