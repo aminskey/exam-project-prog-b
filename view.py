1cf62269ec0a152f7b47c8f597a4190e025b69a3
aminskey <aminskey@github.com>
2025-03-19 17:36:23 +0100

    Selecting a cryptocurrency in the dropdown menu updates the graph

--- view.py
+++ view.py
@@ -44,9 +44,30 @@ class View:
             child.destroy()
 
 
+
+    def update_graph(self, event, names: list, data, w):
+        item = w.get()
+        i = names.index(item)
+
+        plt.clf()
+        for w in self.root.winfo_children():
+            if w.winfo_name() == "graph":
+                w.destroy()
+
+        self.drawPlot(data[i], "dkk")
+        buff = self.plotToImg()
+        img = ImageTk.PhotoImage(buff)
+        lb = Label(self.root, image=img, borderwidth=7, relief="sunken", name="graph")
+        lb.grid(row=0, column=1)
+
+        self.root.mainloop()
+
+
+
+
     def run(self, data, curr):
 
-        names = [i["symbol"].upper() for i in data]
+        names = [i["name"] for i in data]
 
         self.reset()
         self.drawPlot(data[-1], curr)
@@ -54,19 +75,24 @@ class View:
         img = ImageTk.PhotoImage(buff) # buff.resize((buff.width*3//4, buff.height*3//4))
 
         leftColumn = Frame(self.root)
-        infoBox = ui.InfoBox(leftColumn, None, data[-1], relief="raised", height=self.root.winfo_height()//2)
+        infoBox = ui.InfoBox(leftColumn, None, data[-1], relief=RAISED, height=self.root.winfo_height()//2)
 
 
         uname = Label(leftColumn, text="NAME", padx=10, pady=10, font=("Calibri", 25))
-        dropdown = ttk.Combobox(leftColumn, values=names)
+        dropdown = ttk.Combobox(leftColumn, values=names, state="readonly")
+        dropdown.set(names[-1])
+        c_owned = Button(self.root, text="View owned crypto-stocks", padx=5, pady=2, font=("Calibri", 15))
 
-        lb = Label(self.root, image=img, borderwidth=7, relief="sunken")
+        lb = Label(self.root, image=img, borderwidth=7, relief="sunken", name="graph")
         trade = Button(self.root, text="Buy/Sell", padx=5, pady=2, font=("Calibri", 20, "bold"))
+
+        dropdown.bind("<<ComboboxSelected>>", lambda event: self.update_graph(event, names, data, dropdown))
 
         uname.grid(row=0, column=0)
         dropdown.grid(row=1, column=0)
-        infoBox.grid(row=5, column=0, pady=(10, 0))
+        infoBox.grid(row=2, column=0, pady=(10, 0))
         leftColumn.grid(row=0, column=0, sticky="nw", padx=20)
+        c_owned.grid(row=1, column=0, sticky="ne", padx=5, pady=(10, 5))
 
         lb.grid(row=0, column=1)
         trade.grid(row=1, column=1, sticky="ne", pady=(10, 0))
