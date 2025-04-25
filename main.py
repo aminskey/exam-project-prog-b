d3988575de19f61c761330958a3d80587a510796
johanhede <158845236+johanhede@users.noreply.github.com>
2025-04-25 13:27:07 +0200

    removing comments

--- main.py
+++ main.py
@@ -2,34 +2,7 @@ from model import Model
 from controller import Controller
 from view import View
 
-"""
-p = Player("Johan", 100000)
-p2 = Player("Anubhab", 100000)
 
-m = Model()
-v = View()
-c = Controller(m, v)
-
-tmpCoin = Coin("tempCoin", 500)
-byteCoin = Coin("byteCoin", 1900)
-
-p.invest(tmpCoin, 7)
-p2.invest(byteCoin, 2)
-
-tmpCoin.value += 129
-byteCoin.value -= 500
-
-p.sell(tmpCoin, 7)
-p2.sell(byteCoin, 1)
-
-p.printLog()
-print(" ")
-p2.printLog()
-
-m.savePlayer(p)
-m.savePlayer(p2)
-m.save_to_file()
-"""
 
 m = Model()
 v = View()
